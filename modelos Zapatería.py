ccf44c9f351a5c69126eecf46d0c4d8bfa86a9e6
vik0x <vgutierrez@abargon.com>
2016-11-18 15:29:12 -0600

    modelos

---
+++ modelos Zapatería.py
@@ -0,0 +1,40 @@
+class marca(ndb.Model):
+	nombre = ndb.StringProperty()
+
+class tipo_calzado(ndb.Model):
+	nombre = ndb.StringProperty()
+
+class material_calzado(ndb.Model):
+	nombre = ndb.StringProperty()
+
+class temporada(ndb.Model):
+	nombre = ndb.StringProperty()
+
+class genero(ndb.Model):
+	nombre = ndb.StringProperty()
+
+class zapato(ndb.Model):
+	marca = ndb.StructuredProperty(marca)
+	tipo = ndb.StructuredProperty(tipo_calzado)
+	temporada = ndb.StructuredProperty(temporada)
+	material= ndb.StructuredProperty(material_calzado, repeated = True)
+	genero = ndb.StructuredProperty(genero)
+	numero = ndb.IntegerProperty()
+	costo = ndb.FloatProperty()
+	existencia = ndb.IntegerProperty()
+
+class almacen(ndb.Model):
+	zapatos = ndb.StructuredProperty(zapatos)
+	cantidad = ndb.IntegerProperty()
+	fecha = ndb.DateTimeProperty()
+	tipo = BooleanProperty()
+
+class pedido(ndb.Model):
+	mail = ndb.IntegerProperty()
+	fecha = ndb.DateTimeProperty()
+	estatus = ndb.IntegerProperty()
+
+class detalle_pedido(ndb.Model):
+	pedido = ndb.StructuredProperty(pedido)
+	zapato = ndb.StructuredProperty(zapato)
+	cantidad = ndb.IntegerProperty()
